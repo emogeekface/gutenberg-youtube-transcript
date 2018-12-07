83900f97da941a9c223add86dc37940434b17721
kate.england-moore <hello@kewdev.co.uk>
2018-12-07 19:44:16 +0000

    fix styling issues after official 5.0 release

--- src/init.php
+++ src/init.php
@@ -36,7 +36,7 @@ function transcript_block_cgb_block_assets() {
 	wp_enqueue_style(
 		'transcript_block-cgb-style-css', // Handle.
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-blocks' ) // Dependency to include the CSS after it.
+		array( 'wp-editor' ) // Dependency to include the CSS after it.
 		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: filemtime — Gets file modification time.
 	);
 } // End function transcript_block_cgb_block_assets().
